d64965566ee5f587e9df45c0c20e863755f85c70
atacadaoqueirozat-rgb <atacadaoqueirozat@gmail.com>
2025-09-29 14:15:42 -0300

    Add files via upload

---
+++ valite.js
@@ -0,0 +1,267 @@
+ 
+
+
+
+
+const usuarios = [
+  /*RN-1*/
+{
+und:'11',
+login:'ALINE MENEZES',
+pass: 'Senha2025',
+linkd: 'https://docs.google.com/spreadsheets/d/1N5xprvxi-aQHdmu_caPSNuMqsiLrU0yxGbb8g9Jes8E/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'1',
+login:'CLAUDIONE',
+pass: '2604',
+linkd: 'https://docs.google.com/spreadsheets/d/1Vbuq8HdHcuT4ynCb8qZL-rXN96VcFORgEqVw3m7kUUM/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'14',
+login:'francielio',
+pass: '1902',
+linkd: '',
+linkenc:,
+linktab:
+},
+{
+und:'3',
+login:'GRACIELE',
+pass: '@2829',
+linkd: 'https://docs.google.com/spreadsheets/d/1CmnXjGgOnce_30hT9_zfzug62_PRNbYnAce0I-ztpl0/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'19',
+login:'IANCA SABRINA',
+pass: '2252',
+linkd: 'https://docs.google.com/spreadsheets/d/1vpgqeObvmIC-QKVcozAiDSg2FiPfrpQ8wHsXzaibFEQ/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'6',
+login:'José.w',
+pass: '@Wilton91',
+linkd: 'https://docs.google.com/spreadsheets/d/1V6f54F2QYhZgYCzd1DkYQGHqDuRdp4OT9Ki4bMH9Z1k/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'7',
+login:'JOAO VITOR',
+pass: '5680',
+linkd: 'https://docs.google.com/spreadsheets/d/1k1oOq0q7H2ydjzjfdpXt6z_OfNVPbB8jmjA7OYHcAi0/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'10',
+login:'JOSY',
+pass: 'Josy123',
+linkd: 'https://docs.google.com/spreadsheets/d/11yuMKVNjhmEoFfwLGPP1I6XFNSC-3fn_HnJl112sTsI/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'2',
+login:'LUCIVAN',
+pass: '123456',
+linkd: 'https://docs.google.com/spreadsheets/d/1LmtxLMoW-eZUZ-3D8-FCIwbjiNYLdjDtz-ELj_JEMiA/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'10',
+login:'LUCIA.P',
+pass: 'Maria123',
+linkd: 'https://docs.google.com/spreadsheets/d/13cHyjIBZI4SnfAjU68lJ-CY4p9FJsrIi9jLc0aoojiM/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'16',
+login:'Luzinete Medeiros ',
+pass: '2114',
+linkd: '',
+linkenc:,
+linktab:
+},
+{
+und:'19',
+login:'MARIA IZABELA',
+pass: '2128',
+linkd: 'https://docs.google.com/spreadsheets/d/1CHtFRnfjHxnXq3zxcN9GbHF-Khed-EKBZ7pUozLD0jo/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'11',
+login:'MAYARA.PATRICIA',
+pass: 'Senha123',
+linkd: 'https://docs.google.com/spreadsheets/d/1qpWDa42eP7plxK1wT27MllaPCZ4BCG8k2GfZ6HjbCjE/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'9',
+login:'NAIARA',
+pass: '1020',
+linkd: 'https://docs.google.com/spreadsheets/d/1yDZnFG_oy_dE_UqPCbu9qG33GMwjywFW18Xrk5FGE2k/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'3',
+login:'Pablo Weiber ',
+pass: '050394',
+linkd: 'https://docs.google.com/spreadsheets/d/1hFg6y1XSz3NoAi5YaRf5N8zO-VoEC7WSNs5hvM8BRdo/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'17',
+login:'Rita de cassia',
+pass: '12345',
+linkd: '',
+linkenc:,
+linktab:
+},
+{
+und:'1',
+login:'TAWANN',
+pass: '206',
+linkd: 'https://docs.google.com/spreadsheets/d/18UOEmIT3G7jv8G-LQdbdBUte64_aH2Z1ok7tIXT1tc0/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+
+  /*RN-2*/
+{
+und:'13',
+login:'Amanda izilly',
+pass: '410',
+linkd: 'https://docs.google.com/spreadsheets/d/1sRvOFg62Od3HSH5cjc6MtkkR7G9yH_XHJID3HE2uEpU/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'12',
+login:'Ana Karoline do nascimento',
+pass: 'Karol10',
+linkd: 'https://docs.google.com/spreadsheets/d/19Gn7X7JYIxjvJym6p4P9xc_bNVLwxz1Jj1xJrTxRBSg/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'14',
+login:'Carlos magno de Meneses',
+pass: 'CARLOS25@',
+linkd: 'https://docs.google.com/spreadsheets/d/1KAvRZ8q741R_Rvw3iHjre_ahX7jGGfqHccXrrgGen48/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'13',
+login:'Francisca Gislaine pereira',
+pass: '2506',
+linkd: 'https://docs.google.com/spreadsheets/d/1Z9Akmd0wdx_N2dg8i8EnMNVygsVhEnOvWzRMl6IM1qk/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'14',
+login:'Francisco francinildo',
+pass: '1902',
+linkd: 'https://docs.google.com/spreadsheets/d/1lBb3S1twLGbmA2zAhgiBcer65-rTvtu724fpZPc7wFM/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'12',
+login:'GIZELI',
+pass: '123',
+linkd: 'https://docs.google.com/spreadsheets/d/1SLOpdEOWbtt_22OSspB-YMkpOOJMoDilLVK7_yUr0ps/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'13',
+login:'João costa',
+pass: '403620',
+linkd: 'https://docs.google.com/spreadsheets/d/1Rb3QftFYGAhBZsh85xxXdkh54CsLhX-K-8VUUeCCLDs/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'16',
+login:'Luzinete Kátia de Medeiros',
+pass: '2114',
+linkd: 'https://docs.google.com/spreadsheets/d/1VqGPo8Wi9Wz3nbwPZ7puQzOZcILSjjj0Vq8o1bh_yW8/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'20',
+login:'Michael Douglas Mendonça',
+pass: 'Benicio16',
+linkd: 'https://docs.google.com/spreadsheets/d/1e5MqGwAebIsoebM_8p30ykNXOevOEQy3pWgpwjsUMZs/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'12',
+login:'Micharles Gomes da Silva',
+pass: '123',
+linkd: 'https://docs.google.com/spreadsheets/d/1G8x81om2lKW8xpsyPuIp4DKG1iMXKjsRqVsa-CPUhjI/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'20',
+login:'Sheila Cristina Xavier segundo',
+pass: '123',
+linkd: 'https://docs.google.com/spreadsheets/d/1NMGlOlSsylVCiFnWqM9ZjxHTC4JrsmTpZmszVouS4hw/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+{
+und:'13',
+login:'Tallys Ricardo da Silva',
+pass: 'Senha*123456',
+linkd: 'https://docs.google.com/spreadsheets/d/1Bac-niZfirWXKKdAFV6mGtNcew_ChCkOegM4rt0POg0/edit?usp=sharing',
+linkenc:,
+linktab:
+},
+  //supervisores
+{
+und:'41',
+login:'luis',
+pass: '2017',
+linkd:'https://atacadaoqueirozat-rgb.github.io/METAS-GERENCIAL/',
+linkenc:,
+linktab:
+},
+ 
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
